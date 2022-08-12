dbd7415b3432120c5187b8401720c7ff6ad81081
Samitakarke <samitakarke@gmail.com>
2022-08-12 14:58:11 -0400

    created practice.js

---
+++ practice.js
@@ -0,0 +1,170 @@
+// 
+
+// 
+// for(let i=1; i<=10; i++)
+// {
+//     if(i % 2 ===0)
+//     {
+//         console.log(i);
+//     }
+//     else
+//     {
+//         console.log('odd');
+//     }
+// }
+
+// let movies = ['Nausica and the Valley of the Wind','Donnie Darko','Spirited Away']
+// let firstMovie = movies[0]
+// console.log(firstMovie);
+
+// movies.push('The Spongebob Movie');
+// console.log(movies[3]);
+
+// movies.shift()
+// console.log(movies)
+
+// movies.splice(1,0,'Avatar');
+// console.log(movies)
+
+// movies.splice(2,1);
+// console.log(movies);
+
+
+let counter = 10;
+
+// while(counter>=0)
+// {
+//     console.log(counter);
+//     counter -=1;
+// }
+
+
+// 
+// multiCounter =5;
+// while(multiCounter <100)
+// {
+//     console.log(multiCounter);
+//     multiCounter +=5;
+// }
+
+
+// const addNums =(num1, num2,num3) => num1 +num2 +num3
+
+// console.log(addNums(10,20,35));
+
+// const addNumsAndDivide =(num1,num2,num3) =>
+// {
+//     return(num1 +num2 +num3)/3
+// }
+
+// console.log(addNumsAndDivide)
+// const price= 27.83
+// const totalGiven =100
+// const bills =[100,50,20,10,5,1,.25,.10,.05,.01]
+// let change = totalGiven -price
+// const changeArr = []
+// const chackBill= (changeAmount, billAmount)  =>
+// {
+//     return(Math.floor(changeAmount /billAmount))
+// }
+
+// for(bill of bills)
+// {
+//     let numBills = checkBill(change, bill)
+//     change -=Math.round(numBills * bill).toPrecision    
+//     console.log(change)
+// }
+
+
+// function computeArea(width,height)
+// {
+//     return ("The area of a rectangle with a width of"+width+"and a height of"+height +"is" +width*height +"square units."  )
+// }
+
+
+// console.log(computeArea(10,20))
+
+
+// function computeArea(width,height)
+// {
+//     return(`The area of a rectangle with a width of ${width} and a ${height} is ${width*height} square units.`)
+// }
+
+// console.log(computeArea(5,5))
+
+// const planetHasWater =(planet) =>
+// {
+//    if (planet.toLowerCase() === "earth" ) 
+//    {
+
+//     return true
+
+//    }else if(planet==="Mars")
+//    {
+//     return true
+//    }
+//    else
+//    {
+//     return false
+
+//    }
+// }   
+
+//    console.log(planetHasWater("Mars"))
+
+// function addList()
+// {
+    
+//     let sum = 0
+//     for(let i = 0; i < arguments.length; i++)
+//     {
+//         sum = sum +arguments[i]
+//     }
+//     console.log(sum)
+//     return sum;
+// }    
+
+
+//     // addList(3,2);
+//     addList(2,3,4,5);
+
+
+// var favoriteRecipe = {
+//     title: 'Mole',
+//     serves:2,
+//     ingredients:['cumin','cinnamon','cocoa']
+// }
+
+// console.log(favoriteRecipe.title);
+// console.log('Serves: ' + favoriteRecipe.serves);
+// console.log('ingredients');
+// for ( let i =0; i <favoriteRecipe.ingredients.length;i++)
+// {
+//     console.log(favoriteRecipe.ingredients[i]);
+// }
+
+// const books =[
+// {
+
+//     title: 'Eragon'
+//     author: 'Christopher Paolini'
+//     alreadyRead: true
+// }
+// ]
+
+// class Character 
+// {
+//     greet()
+//     {
+//         console.log
+//     }
+// }
+
+// Person
+// {
+//     constructor(interactive, height, helpful)
+//     {
+        
+//     }
+// }
+
