b0de995a29cdbe73b9fa9489ef3dbdeb74249257
sudeepsubedi <sudeep.subedi@ufl.edu>
2022-08-13 10:05:02 -0400

    add variable a for testing purpose

--- new_practice.js
+++ new_practice.js
@@ -2,11 +2,13 @@ let x = 0;
 
 console.log(x);
 
-<<<<<<< HEAD
 let z = "test";
 console.log(z);
-=======
 
-let y=5;
+let y = 5;
 console.log(y);
->>>>>>> a39e7ff62b7e7d160fd3de86dfdda6331adf34be
+
+console.log(2 * 5);
+
+let a = "What";
+console.log(a);
